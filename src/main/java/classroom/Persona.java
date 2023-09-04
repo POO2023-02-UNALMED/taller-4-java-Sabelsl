4c6377d9529b6e12b18b1da8494bfe942db681e4
Sabelsl <141966602+Sabelsl@users.noreply.github.com>
2023-09-04 17:22:27 -0500

    :abc:

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -11,7 +11,8 @@ public class Persona {
     }
     
     public Persona() {
-    	this("", 0);
+    	this.nombre = "";
+    	totalPersonas++;
     }
 
     public Persona(long cedula, String nombre) {
@@ -33,7 +34,7 @@ public class Persona {
     }
 
     public Persona(String nombre) {
-        this.nombre = "";
+        this.nombre = nombre;
         this.cedula = 1;
         totalPersonas++;
     }
